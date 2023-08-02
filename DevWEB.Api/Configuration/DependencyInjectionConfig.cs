d3f1d495dccfb0be4d018a52dc4008b0cbfe5491
Gabriel Silva <gabrielgbr.contato@hotmail.com>
2023-08-01 22:11:02 -0300

    Alterações gerais

---
+++ DevWEB.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,25 @@
+﻿using DevIO.Business.Notificacoes;
+using DevWEB.Business.Intefaces;
+using DevWEB.Business.Services;
+using DevWEB.Data.Context;
+using DevWEB.Data.Repository;
+
+namespace DevWEB.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ContextDB>();
+            services.AddScoped<IFornecedorRepository, FornecedorRepository>();
+            services.AddScoped<IProdutoRepository, ProdutoRepository>();
+            services.AddScoped<IEnderecoRepository, EnderecoRepository>();
+
+            services.AddScoped<INotificador, Notificador>();
+            services.AddScoped<IFornecedorService, FornecedorService>();
+            services.AddScoped<IProdutoService, ProdutoService>();
+
+            return services;
+        }
+    }
+}
---
+++ DevWEB.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using DevWEB.Api.DTOs;
+using DevWEB.Business.Intefaces;
+using DevWEB.Business.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevWEB.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorService _fornecedorService;
+        private readonly IMapper _mapper;
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IFornecedorService fornecedorService, IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+            _fornecedorService = fornecedorService;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FornecedorDTO>>> ObterTodos()
+        {
+            return Ok(_mapper.Map<IEnumerable<FornecedorDTO>>(await _fornecedorRepository.ObterTodos()));
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorDTO>> ObterPorId(Guid id)
+        {
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+
+            if (fornecedor == null) NotFound();
+
+            return Ok(fornecedor);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorDTO>> Adicionar(FornecedorDTO model)
+        {
+            if (!ModelState.IsValid) BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(model);
+
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return Ok();
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorDTO>> Adicionar(Guid id, FornecedorDTO model)
+        {
+            if (id != model.Id) BadRequest();
+
+            if (!ModelState.IsValid) BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(model);
+
+            await _fornecedorService.Atualizar(fornecedor);
+
+            return Ok(fornecedor);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorDTO>> Excluir(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor != null) BadRequest();
+
+            await _fornecedorService.Remover(id);
+
+            return Ok();
+        }
+
+        private async Task<FornecedorDTO> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorDTO>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+        private async Task<FornecedorDTO> ObterFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorDTO>(await _fornecedorRepository.ObterFornecedorEndereco(id));
+        }
+    }
+}
